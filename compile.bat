08e3c225de72174123c09cfdabd470d03a341162
Eidalac <eidalac@gmail.com>
2021-04-26 14:49:20 -0500

    Updates
    Compiler update
    fixed some typos.
    removed depreciated files.

--- compile.bat
+++ compile.bat
@@ -1,14 +1,1 @@
-@echo off
-:: Free Cities Basic Compiler - Windows
-
-:: Set working directory
-pushd %~dp0
-
-:: Run the appropriate compiler for the user's CPU architecture.
-if %PROCESSOR_ARCHITECTURE% == AMD64 (
-    CALL "%~dp0devTools\tweeGo\tweego_win64.exe" -o "%~dp0bin/FluffyBreeder.html" "%~dp0src"
-) else (
-    CALL "%~dp0devTools\tweeGo\tweego_win86.exe" -o "%~dp0bin/FluffyBreeder.html" "%~dp0src"
-)
-popd
-ECHO Done
+bash -c "/mnt/e/Users/Eidalac/eclipse-workspace2/FluffyBreeder/compile.sh"
